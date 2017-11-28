af7ef3fe594831dac69e4d0d82657f09c5dfad92
ju <begottiju@gmail.com>
2017-11-28 14:54:41 -0200

    só faltam os testes

--- app/src/main/java/com/learn2fantasy/learn2fantasy/MainActivity.java
+++ app/src/main/java/com/learn2fantasy/learn2fantasy/MainActivity.java
@@ -1,6 +1,7 @@
 package com.learn2fantasy.learn2fantasy;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         BD.testaBD(this);
+
+        final int MILISEGUNDOS = 3000;
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(i);
+            }
+        }, MILISEGUNDOS);
     }
 
     public void irMenu(View view) {
